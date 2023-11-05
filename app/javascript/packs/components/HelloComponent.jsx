f307a39c408f3b482ac20648ebd5880b61fd846a
filip19992 <filipwlodarczyk99@gmail.com>
2023-11-05 17:05:27 +0100

    HelloComponent with call to actual api

--- app/javascript/packs/components/HelloComponent.jsx
+++ app/javascript/packs/components/HelloComponent.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function HelloComponent() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('/api/hello')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);
+
   return (
     <div>
-      <p>Hello from React component!</p>
+      <p>{message}</p>
     </div>
   );
 }
