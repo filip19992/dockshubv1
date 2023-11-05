82f0cef1340d5b9c10c6d7aa07ffff931519da7d
filip19992 <filipwlodarczyk99@gmail.com>
2023-11-05 17:04:09 +0100

    working HelloComponent

---
+++ app/javascript/packs/components/HelloComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HelloComponent() {
+  return (
+    <div>
+      <p>Hello from React component!</p>
+    </div>
+  );
+}
+
+export default HelloComponent;
--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,28 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import { createRoot } from 'react-dom';
+import HelloComponent from './components/HelloComponent';
 
-function HelloComponent() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    axios.get('/api/hello')
-      .then(response => {
-        setMessage(response.data.message);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }, []);
-
-  return (
-    <div>
-      <p>{message}</p>
-    </div>
-  );
-}
-
-const root = createRoot(document.getElementById('hello-message'));
-root.render(<HelloComponent />);
-
-export default HelloComponent;
+document.addEventListener('DOMContentLoaded', () => {
+  const reactRoot = document.getElementById('react-root');
+  const root = createRoot(reactRoot);
+  root.render(<HelloComponent />);
+});
