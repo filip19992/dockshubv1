3d4a0464e20868c9fc3983c358d9e909024ff76c
filip19992 <filipwlodarczyk99@gmail.com>
2023-12-16 15:38:42 +0100

    Clean up of test methods

--- app/javascript/packs/hello_react.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom';
-import HelloComponent from './components/HelloComponent';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const reactRoot = document.getElementById('react-root');
-  const root = createRoot(reactRoot);
-  root.render(<HelloComponent />);
-});
