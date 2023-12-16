33a660e40c791d1fc53de4747d04438da9f46056
filip19992 <filipwlodarczyk99@gmail.com>
2023-12-16 16:31:33 +0100

    Fixing typo

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'layouts#index'
   delete 'api/file/delete/:filename', to: 'file_uploads#delete'
   post '/api/upload', to: 'file_uploads#create'
-  get '/api/file/get', to: 'file_uploads#index'"
+  get '/api/file/get', to: 'file_uploads#index'
 end
